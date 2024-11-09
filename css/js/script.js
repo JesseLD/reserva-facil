6630bf02d3ee0e45fc75c966f015f79ab53c927e
JONATAS <jonatas.tz10@gmail.com>
2024-11-09 14:30:56 -0300

    Coleta de esboço jessé - seguir esse layout

--- css/js/script.js
+++
@@ -1,9 +0,0 @@
-// Adiciona um evento ao botão de registro
-document.addEventListener('DOMContentLoaded', function() {
-    const registerButton = document.getElementById('register-btn');
-
-    // Adiciona um evento de clique ao botão de registro
-    registerButton.addEventListener('click', function() {
-        window.location.href = 'cadastrar.html'; // Redireciona para a página de cadastro
-    });
-});
