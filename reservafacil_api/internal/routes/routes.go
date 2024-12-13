0e02c30a183012198470db14028ddef0741c90fd
root <root@DESKTOP-JMGEHF9>
2024-12-13 11:21:33 -0300

    Api Fix

--- reservafacil_api/internal/routes/routes.go
+++ reservafacil_api/internal/routes/routes.go
@@ -8,10 +8,17 @@ import (
 )
 
 func SetupRoutes(router *gin.Engine) {
+
+	mainRoutes := router.Group("/")
+	mainRoutes.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to the ReservaFacil API! Documentation is available at https://platform.reservafacil.site/docs/api-reference",
+		})
+	})
+
 	router.Use(middleware.CheckAPIKey)
 
 	user.SetupUserRoutes(router)
-
 
 	router.NoRoute(func(c *gin.Context) { 
 		response.SendResponse(c, "Not Found", 404, nil)
