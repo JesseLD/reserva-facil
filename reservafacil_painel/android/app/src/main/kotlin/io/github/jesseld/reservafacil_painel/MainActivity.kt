b3cb46dae793143b9aaf0f31bc91f18bdd26c703
Turbobot <turbobot@vercel.com>
2024-12-17 09:37:25 -0300

    feat(create-turbo): apply official-starter transform

--- reservafacil_painel/android/app/src/main/kotlin/io/github/jesseld/reservafacil_painel/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package io.github.jesseld.reservafacil_painel
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- reservafacil_painel/lib/core/shared/app_images.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppImages {
-  static get logo => AssetImage("images/logo.png");
-  static get logo_2 => AssetImage("images/logo2.png");
-  static get logo_center => AssetImage("images/logo_center.png");
-}
